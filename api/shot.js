d2718b675debbb3ea62c55018bc24ef2e5898e7d
elianiva <dicha.arkana03@gmail.com>
2021-08-10 12:28:50 +0700

    君の運命のヒトは僕じゃない

---
+++ api/shot.js
@@ -0,0 +1,38 @@
+import { screenshot } from '../utils/screenshot.js';
+import logger from '../utils/logger.js';
+
+export default function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'HEAD, POST, PUT, PATCH');
+  res.setHeader('Access-Control-Allow-Headers', ['content-type', 'accept']);
+
+  const { code, lang, username } = req.body;
+
+  let err = [];
+
+  if (!code) err.push('`code`');
+  if (!lang) err.push('`lang`');
+  if (!username) err.push('`username`');
+
+  if (err.length > 0) {
+    return res.status(400).json({ msg: err.join(' + ') + ' body parameter is required!' });
+  }
+
+  (async () => {
+    try {
+      const base64 = await screenshot(code, lang, username);
+      const image = Buffer.from(base64, 'base64');
+      res.setHeader('Content-Length', image.length);
+      res.setHeader('Content-Type', 'image/png');
+      return res.send(image);
+    } catch (err) {
+      console.log(err);
+      logger.captureException(err, (scope) => {
+        scope.setContext('detail', { lang, username, code });
+        scope.setTags({ lang, username });
+      });
+
+      return res.status(500).json({ msg: 'Something went wrong on our side.' });
+    }
+  })();
+}
---
+++ utils/screenshot.js
@@ -0,0 +1,44 @@
+import puppeteer from 'puppeteer-core';
+import chrome from 'chrome-aws-lambda';
+import { makeHtml } from './makeHtml.js';
+import { getHighlightedCode } from './getHighlightedCode.js';
+
+/** @type {() => Promise<import("puppeteer-core").LaunchOptions>} */
+const getConfig = async () => ({
+  headless: chrome.headless,
+  defaultViewport: chrome.defaultViewport,
+  args: process.env?.EXEC_PATH
+    ? [
+      '--hide-scrollbars',
+      '--disable-web-security',
+      // commented this out because it didn't work on my machine as well
+      // see: https://github.com/puppeteer/puppeteer/issues/1837
+      '--no-sandbox',
+      // '--disable-setuid-sandbox',
+      // '--disable-gpu',
+    ]
+    : chrome.args,
+  executablePath: process.env?.EXEC_PATH || (await chrome.executablePath),
+  ignoreHTTPSErrors: true,
+});
+
+/**
+ * Generate beautiful code snippet screenshot
+ * @param {string} code - Raw code
+ * @param {string} lang - Language
+ * @param {string} username - For window title
+ */
+export const screenshot = async (code = '', lang = 'txt', username = '') => {
+  const browser = await puppeteer.launch(await getConfig());
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080 });
+  await page.setContent(makeHtml(await getHighlightedCode(code, lang), username));
+
+  await page.waitForSelector('.container');
+  const container = await page.$('.container');
+
+  const image = await container.screenshot({ encoding: 'base64' });
+  await page.close();
+  // await browser.close();
+  return image;
+};
