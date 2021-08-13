027dee8b8bd7fe9d07197d807d0b10474bf6faa8
Reinaldy Rafli <aldy505@tutanota.com>
2021-08-14 01:00:05 +0700

    you got it mann but you fkn around
    https://www.youtube.com/watch?v=AnA6IYpsWYE

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import { screenshot } from './utils/shot.js';
+import { screenshot } from './utils/screenshot.js';
 import logger from './utils/logger.js';
 
 const corsDefault = {
@@ -14,60 +14,67 @@ const server = http.createServer((req, res) => {
     data += chunk;
   });
   req.on('end', () => {
-    if (req?.method?.toUpperCase() === 'OPTIONS') {
-      return res.writeHead(204, { 'Content-Length': 0, ...corsDefault }).end();
-    }
-
-    if (!data) {
-      return res
-        .writeHead(400, {
-          'Content-Type': 'application/json',
-          ...corsDefault,
-        })
-        .end(JSON.stringify({ msg: 'empty body is not allowed!!' }));
-    }
-
-    const { code, lang, username } = JSON.parse(data);
-    let err = [];
-
-    if (!code) err.push('`code`');
-    if (!lang) err.push('`lang`');
-    if (!username) err.push('`username`');
-
-    if (err.length > 0) {
-      return res
-        .writeHead(400, {
-          'Content-Type': 'application/json',
-          ...corsDefault,
-        })
-        .end(JSON.stringify({ msg: err.join(' + ') + ' body parameter is required!' }));
-    }
-
-    (async () => {
-      try {
-        const base64 = await screenshot(code, lang, username);
-        const image = Buffer.from(base64, 'base64');
-        return res
-          .writeHead(200, {
-            'Content-Length': image.length,
-            'Content-Type': 'image/png',
-            ...corsDefault,
-          })
-          .end(image);
-      } catch (err) {
-        logger.captureException(err, (scope) => {
-          scope.setContext('detail', { lang, username, code, payload: data });
-          scope.setTags({ lang, username });
-        });
-
-        return res
-          .writeHead(500, {
-            'Content-Type': 'application/json',
-            ...corsDefault,
-          })
-          .end(JSON.stringify({ msg: 'Something went wrong on our side.' }));
+    switch (req?.method?.toUpperCase()) {
+      case 'OPTIONS': {
+        return res.writeHead(204, { 'Content-Length': 0, ...corsDefault }).end();
       }
-    })();
+      case 'GET': {
+        return res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' }).end('<h1>coming soon!</h1>', 'utf-8');
+      }
+      default: {
+        if (!data) {
+          return res
+            .writeHead(400, {
+              'Content-Type': 'application/json',
+              ...corsDefault,
+            })
+            .end(JSON.stringify({ msg: 'empty body is not allowed!!' }));
+        }
+
+        const { code, lang, username } = JSON.parse(data);
+        const err = [];
+
+        if (!code) err.push('`code`');
+        if (!lang) err.push('`lang`');
+        if (!username) err.push('`username`');
+
+        if (err.length > 0) {
+          return res
+            .writeHead(400, {
+              'Content-Type': 'application/json',
+              ...corsDefault,
+            })
+            .end(JSON.stringify({ msg: err.join(' + ') + ' body parameter is required!' }));
+        }
+
+        (async () => {
+          try {
+            const base64 = await screenshot(code, lang, username);
+            const image = Buffer.from(base64, 'base64');
+            return res
+              .writeHead(200, {
+                'Content-Length': image.length,
+                'Content-Type': 'image/png',
+                ...corsDefault,
+              })
+              .end(image);
+          } catch (err) {
+            logger.captureException(err, (scope) => {
+              scope.setContext('detail', { lang, username, code, payload: data });
+              scope.setTags({ lang, username });
+              return scope;
+            });
+
+            return res
+              .writeHead(500, {
+                'Content-Type': 'application/json',
+                ...corsDefault,
+              })
+              .end(JSON.stringify({ msg: 'Something went wrong on our side.' }));
+          }
+        })();
+      }
+    }
   });
 });
 
