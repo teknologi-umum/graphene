93da5c12e9a55ca03b17c601e069c68342c85ed3
Reinaldy Rafli <aldy505@tutanota.com>
2021-08-19 21:12:44 +0700

    *saxophone noises*

--- src/static/main.js
+++ src/static/main.js
@@ -1,1 +1,3 @@
-console.log(`Hi! If you saw this, that means you are a web enthusiast.\nConsider helping the development of this website on Github: https://github.com/teknologi-umum/graphene\n\nSee you!`);
+console.log(
+  `Hi! If you saw this, that means you are a web enthusiast.\nConsider helping the development of this website on Github: https://github.com/teknologi-umum/graphene\n\nSee you!`,
+);
