ab70c119611416f96dda93a05f68ef2bae47ba0b
elianiva <dicha.arkana03@gmail.com>
2021-09-23 19:23:55 +0700

    moshi moshi

--- src/logic/validate.ts
+++ src/logic/validate.ts
@@ -1,21 +1,42 @@
 import { BUNDLED_THEMES } from 'shiki';
 import type { ValidateFuncOptions } from '../types/function';
 
+type Validator = <T>(name: string, what: T) => string;
+type ValidatorWithKeys = <T>(name: string, what: T, keys: T[], err?: string) => string;
+
+const requiredString: Validator = (name, what) => (what ? '' : `${name} is required`);
+
+const requiredNumber: Validator = (name, what) => {
+  if (!what) return `${name} is required!`;
+  if (typeof what !== 'number') return `${name} must be a number!`;
+  if (what < 1) return `${name} can't be lower than 1!`;
+  return '';
+};
+
+const requiredKeys: ValidatorWithKeys = (name, what, keys, err) => {
+  // @ts-ignore 「Intl.ListFormat」がまだないから
+  const lf = new Intl.ListFormat('en');
+  if (!what) return `${name} is required!`;
+  if (!keys.includes(what)) return err ? err : `Bad \`${name}\`! Valid options are ${lf.format(keys)}`;
+  return '';
+};
+
 /**
  * Validate valid options
  * @param {ValidateFuncOptions} validate
  */
-export const validate = ({ code, upscale, format, theme, font }: ValidateFuncOptions): string[] => {
+export const validate = ({ code, upscale, format, theme, font, border }: ValidateFuncOptions): string[] => {
   return [
-    !code && '`code` is required!',
-    upscale && typeof upscale !== 'number' && '`upscale` must be a number!',
-    upscale < 1 && "`upscale` can't be lower than 1!",
-    format && !['png', 'jpeg', 'webp'].includes(format) && 'Bad `format`! Valid options are `png`, `jpeg`, and `webp`',
-    theme &&
-      !BUNDLED_THEMES.includes(theme) &&
+    requiredString('code', code),
+    requiredNumber('upscale', upscale),
+    requiredNumber('border', border),
+    requiredKeys('format', format, ['png', 'jpeg', 'webp']),
+    requiredKeys(
+      'theme',
+      theme,
+      BUNDLED_THEMES,
       'Bad `theme`! See https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-themes for list of valid themes',
-    font &&
-      !['sf mono', 'jetbrains mono', 'fira code'].includes(font.toLowerCase()) &&
-      'Bad `font`! Valid options are `sf mono`, `jetbrains mono`, and `fira code`',
+    ),
+    requiredKeys('font', font?.toLowerCase(), ['sf mono', 'jetbrains mono', 'fira code']),
   ].filter(Boolean) as string[];
 };
