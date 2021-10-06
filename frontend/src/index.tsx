8a4da43247b49979462e651ae80cad2b33dbaf54
Reinaldy Rafli <aldy505@tutanota.com>
2021-10-06 18:42:28 +0700

    passages

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import { render } from 'solid-js/web';
 import '@fontsource/rubik/400.css';
 import '@fontsource/rubik/500.css';
 import '@fontsource/rubik/700.css';
+import '@fontsource/jetbrains-mono/400.css';
 import App from './App';
 import './global.css';
 
