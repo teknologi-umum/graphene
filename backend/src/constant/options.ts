0568d0d0aca847a4a736b9feef986bfd6db6b550
elianiva <dicha.arkana03@gmail.com>
2021-11-06 08:18:55 +0700

    会いたいけど。。

---
+++ backend/src/constant/options.ts
@@ -0,0 +1,63 @@
+import { BUNDLED_LANGUAGES, BUNDLED_THEMES } from 'shiki';
+
+export type Option = {
+  type: 'string' | 'number' | 'boolean' | 'keys';
+  isRequired: boolean;
+  min?: number;
+  max?: number;
+  options?: string[];
+  errorMessage?: string;
+};
+
+type Options = Record<string, Options | Option>;
+
+export const VALID_OPTIONS: Options = {
+  lineNumber: {
+    type: 'boolean',
+  },
+  code: {
+    type: 'string',
+    isRequired: true,
+  },
+  upscale: {
+    type: 'number',
+    isRequired: false,
+    min: 1,
+    max: 5,
+  },
+  border: {
+    thickness: {
+      type: 'number',
+      isRequired: false,
+      min: 0,
+      max: Infinity,
+    },
+    colour: {
+      type: 'string',
+      isRequired: false,
+    },
+  },
+  format: {
+    type: 'keys',
+    isRequired: false,
+    options: ['png', 'jpeg', 'webp', 'svg'],
+  },
+  lang: {
+    type: 'keys',
+    isRequired: false,
+    options: BUNDLED_LANGUAGES.map((l) => l.id),
+    errorMessage: 'Bad `lang`! See https://github.com/shikijs/shiki/blob/main/docs/languages.md#all-languages',
+  },
+  theme: {
+    type: 'keys',
+    isRequired: false,
+    options: BUNDLED_THEMES,
+    errorMessage:
+      'Bad `theme`! See https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-themes for list of valid themes',
+  },
+  font: {
+    type: 'keys',
+    isRequired: false,
+    options: ['sf mono', 'jetbrains mono', 'fira code', 'hack', 'iosevka', 'cascadia code'],
+  },
+};
--- backend/src/logic/validate.ts
+++ backend/src/logic/validate.ts
@@ -1,71 +1,30 @@
-import { BUNDLED_LANGUAGES, BUNDLED_THEMES } from 'shiki';
+import { Option, VALID_OPTIONS } from '../constant/options';
 import type { ValidOptions } from '../types/function';
-
-const validateBoolean = (name: string, bool: boolean): string => {
-  if (bool !== undefined && bool !== null && typeof bool !== 'boolean') return `\`${name}\` must be a boolean!`;
-  return '';
-};
-
-const validateString = (name: string, str: string, required: boolean): string => {
-  if (!str && required) return `\`${name}\` is required!`;
-  if (str && typeof str !== 'string') return `${name} must be a string!`;
-  return '';
-};
-
-const validateNumber = (name: string, num: number, min: number, max: number, required: boolean): string => {
-  if (!num && required) return `\`${name}\` is required!`;
-
-  if (num === 0 || num) {
-    if (typeof num !== 'number') return `\`${name}\` must be a number!`;
-    if (!(num >= min && num <= max)) return `\`${name}\` can't be lower than ${1} or higher than ${max}!`;
-  }
-
-  return '';
-};
-
-const validateKeys = <T>(name: string, key: T, keys: T[], err: string, required: boolean) => {
-  if (!key && required) return `${name} is required!`;
-
-  // @ts-ignore 「Intl.ListFormat」のタイプがまだないから
-  const lf = new Intl.ListFormat('en');
-  if (key && !keys.includes(key))
-    return err ? err : `Bad \`${name}\`! Valid options are ${lf.format(keys.map((x) => `\`${x}\``))}`;
-
-  return '';
-};
+import { resolve } from '../utils/resolve';
+import { validateBoolean, validateKeys, validateNumber, validateString } from '../utils/validator';
 
 /**
  * Validate valid options
  * @param {ValidateFuncOptions} validate
  */
-export const validate = ({ code, upscale, format, theme, font, border, lang, lineNumber }: ValidOptions): string[] => {
-  return [
-    validateBoolean('lineNumber', lineNumber),
-    validateString('code', code, true),
-    validateNumber('upscale', upscale, 1, 5, false),
-    validateNumber('border.thickness', border?.thickness, 0, Infinity, false),
-    validateString('border.colour', border?.colour, false),
-    validateKeys('format', format, ['png', 'jpeg', 'webp', 'svg'], '', false),
-    validateKeys(
-      'lang',
-      lang,
-      BUNDLED_LANGUAGES.map((l) => l.id),
-      'Bad `lang`! See https://github.com/shikijs/shiki/blob/main/docs/languages.md#all-languages',
-      false,
-    ),
-    validateKeys(
-      'theme',
-      theme,
-      BUNDLED_THEMES,
-      'Bad `theme`! See https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-themes for list of valid themes',
-      false,
-    ),
-    validateKeys(
-      'font',
-      (font ?? '').toLowerCase(),
-      ['sf mono', 'jetbrains mono', 'fira code', 'hack', 'iosevka', 'cascadia code'],
-      '',
-      false,
-    ),
-  ].filter(Boolean) as string[];
+export const validate = (opts: ValidOptions): string[] => {
+  return Object.keys(VALID_OPTIONS)
+    .map((k) => {
+      const optVal = resolve(opts, k);
+      const current: Option = VALID_OPTIONS[k];
+
+      switch (current.type) {
+        case 'boolean':
+          return validateBoolean(k, optVal);
+        case 'string':
+          return validateString(k, (optVal || '').toLowerCase(), current.isRequired);
+        case 'number':
+          return validateNumber(k, optVal, current.min as number, current.max as number, current.isRequired);
+        case 'keys':
+          return validateKeys(k, optVal, current.options as string[], current.errorMessage || '', current.isRequired);
+        default:
+          return '';
+      }
+    })
+    .filter(Boolean) as string[];
 };
