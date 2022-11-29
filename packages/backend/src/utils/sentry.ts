5706af13f7f7c49be6323487c36fd070493b9238
elianiva <dicha.arkana03@gmail.com>
2022-11-29 12:17:32 +0700

    chore: remove note comment

--- packages/backend/src/utils/sentry.ts
+++ packages/backend/src/utils/sentry.ts
@@ -3,7 +3,6 @@ import "@sentry/tracing";
 import { IS_PRODUCTION, NODE_ENV, SENTRY_DSN } from "~/constants";
 
 Sentry.init({
-  // @ts-expect-error -- not sure why it's complaining about missing type definition
   dsn: SENTRY_DSN,
   enabled: IS_PRODUCTION,
   environment: NODE_ENV,
